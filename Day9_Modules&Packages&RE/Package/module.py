ac1026f4790b38712921d95e8e6ae419df76c0f3
LavanyaPolamarasetty <51402194+LavanyaPolamarasetty@users.noreply.github.com>
2021-06-09 16:14:55 +0530

    Add files via upload

---
+++ Day9_Modules&Packages&RE/Package/module.py
@@ -0,0 +1,17 @@
+def hi(name):
+    return "hiii my dear"+name
+
+import calendar
+def IsLeap(year):
+    return calendar.isleap(year)
+
+def sums(a,b,c,d):
+    return a+b+c+d
+
+def add(*args):
+    s = 0
+    for i in args:
+        s += i
+    return s
+                        
+    
